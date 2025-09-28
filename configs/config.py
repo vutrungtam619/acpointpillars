1bb60f80b32a196dd2b9d75bcdef89342d95b7ed
vutrungtam619 <vutrungtam619@gmail.com>
2025-09-28 19:41:09 +0700

    Add

---
+++ configs/config.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+root = root = Path(__file__).parent.parent
+
+config = {
+    'data_root': 'Kitti',
+    'checkpoint_dir': Path(root) / 'checkpoints',
+    'log_dir': Path(root) / 'logs',
+    'classes': {'Pedestrian': 0, 'Cyclist': 1, 'Car': 2},
+    'num_classes': 3,
+    'point_cloud_range': [0, -39.68, -3, 69.12, 39.68, 1],
+    'voxel_size': [0.16, 0.16, 4],
+    'max_voxels': (12000, 12000),
+    'max_points': 64,
+    'batch_size_train': 4,
+    'batch_size_val': 4,
+    'num_workers': 4,
+    'init_lr': 0.00025,
+    'epoch': 20,
+    'ckpt_freq': 2,
+    'log_freq': 25,
+}
---
+++ models/__init__.py
@@ -0,0 +1,2 @@
+from .anchor import Anchors, anchors2bboxes, bboxes2deltas
+from .pointpillars import PointPillars
